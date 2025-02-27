6bebc6851ac1e0bec3a51aaea45305b61b968817
Squaad6z <carneiromoreirasteven22@gmail.com>
2025-02-28 00:55:31 +0100

    Modification avis

--- src/app/components/Avis/Avis.jsx
+++ src/app/components/Avis/Avis.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
-import './avis.css'
+import React from 'react';
+import './avis.css';
 
-const Avis = () => {
+const stars = (rating) => {
+  const starFull = '★';
+  const starEmpty = '☆';
+  return Array.from({ length: 5 }, (_, index) =>
+    index < rating ? starFull : starEmpty
+  ).join(' ');
+};
+
+const ReviewCard = ({ review }) => {
   return (
-    
-    <section id='avis' className='avis_container'>
-    <script src="https://static.elfsight.com/platform/platform.js" async></script>
-    <div class="elfsight-app-264c4d90-c4e4-4e4d-b756-0d7ab9ab914b"></div>    
+    <section className="reviewCard">
+      <aside className="stars">{stars(review.rating)}</aside>
+      <p className="reviewText">{review.text}</p>
+      <p className="author">- {review.author}</p>
     </section>
-  )
-}
+  );
+};
 
-export default Avis
+const AvisCard = () => {
+  const reviews = [
+    {
+      rating: 5,
+      text: "Concepteur de confiance et très professionnel. Il est à l'écoute et très réactif. Patient et performant. Je recommande vivement. Je suis très satisfaite de son travail et je peux commencer mon activité avec un site internet au top. Merci beaucoup.",
+      author: "Christelle SILVA - L'espace d'un Instant ",
+    },
+    {
+      rating: 5,
+      text: "Un grand merci à CodeMeta Solutions pour cette belle collaboration et l'aboutissement du projet Neuf Mois Demain! Beaucoup de travail, beaucoup de sérieux et une grande écoute des besoins des clients, nous recommandons CodeMeta Solutions à 200%!!!",
+      author: "Séverine CARNEIRO MOREIRA - Neuf Mois Demain ",
+    },
+    {
+      rating: 5,
+      text: "CodeMeta Solutios a créé mon site internet et je suis très satisfait du résultat. Il est professionnel, à l’écoute et a su répondre à toutes mes attentes. Partagez lui vos idées et ce que vous aimez et il créera un site à votre image. Je le recommande sans hésitation pour toute création de site web.",
+      author: "Greenz Ink Tattoo",
+    },
+    {
+      rating: 5,
+      text: "Création top, réactif, prix agressif, vraiment satisfaisant. Je recommande",
+      author: "Alexandre Laurain - Sonergie",
+    },
+    {
+      rating: 5,
+      text: "Super professionnel rien à dire. Je recommande à 100%",
+      author: "Precillia Claire",
+    },
+  ];
+
+  return (
+    <section className='container_avis'>
+      <h1 className="title">Avis de Nos Clients</h1>
+      <section className="container">
+        {reviews.map((review, index) => (
+          <ReviewCard key={index} review={review} />
+        ))}
+      </section>
+    </section>
+  );
+};
+
+export default AvisCard;
